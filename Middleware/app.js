ca760a60f92410df52d667ac9bf18c9446f5afcb
Babu Nair <babunairofficial@gmail.com>
2025-10-30 21:49:46 +0530

    api token as query string

--- Middleware/app.js
+++ Middleware/app.js
@@ -12,6 +12,18 @@ app.use("/random", (req, res, next) => {
     next();
 });
 
+app.use("/api", (req, res, next) => {
+    let {token} = req.query;
+    if(token === "giveaccess") {
+        next();
+    }
+    res.send("ACCESS DENIED!");
+});
+
+app.get("/api", (req, res) => {
+    res.send("data");
+});
+
 app.get("/", (req, res) => {
     res.send("Hi, I am root.")
 });
