ca7bfbd4a608d7ee00e268dc280af9f992a8403c
Babu Nair <babunairofficial@gmail.com>
2025-10-31 18:43:53 +0530

    multiple middlewares

--- Middleware/app.js
+++ Middleware/app.js
@@ -12,15 +12,16 @@ app.use("/random", (req, res, next) => {
     next();
 });
 
-app.use("/api", (req, res, next) => {
+//multiple middlewares
+const checkToken = (req, res, next) => {
     let {token} = req.query;
     if(token === "giveaccess") {
         next();
     }
     res.send("ACCESS DENIED!");
-});
+};
 
-app.get("/api", (req, res) => {
+app.get("/api", checkToken, (req, res) => { //multiple middlewares used here
     res.send("data");
 });
 
